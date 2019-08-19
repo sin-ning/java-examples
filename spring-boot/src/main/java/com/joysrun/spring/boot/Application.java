7e57216fcb67d3b05a8dd8ee04e488225e040e88
sin-ning <sinning@aliyun.com>
2019-08-19 10:54:37 +0800

    - jstack 调试

---
+++ spring-boot/src/main/java/com/joysrun/spring/boot/Application.java
@@ -0,0 +1,16 @@
+package com.joysrun.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: sin
+ * time: 2019-08-19 10:46
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
